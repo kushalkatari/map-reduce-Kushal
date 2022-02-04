29de5900216f1707aaa37ece1fed0f3d140ec1d0
kushalkatari <kushalkatari@gmail.com>
2022-02-04 13:34:36 -0600

    updated

--- 22mapper.py
+++ 22mapper.py
@@ -2,9 +2,12 @@ import sys
 
 # iterate through each line provided via standard input
 for line in sys.stdin:
+  print(line)    
   datalist = line.strip().split(",")
-  if (len(datalist) ==2) : 
-    title,rating = datalist
+  print(datalist)
+  if (len(datalist) ==3): 
+    title,rating,ratingLevel = datalist
+    print(rating)
 
     # print intermediate key-value pairs to standard output
     print(rating,"\t",1)
